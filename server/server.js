8234a72660f4229f3a4b5dc9ce06df8b6f83e88f
juleschatt-hub <72096647+juleschatt-hub@users.noreply.github.com>
2024-05-02 17:53:04 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5001;
+app.use(express.static('server/public'));
+
+app.listen(port, () => {
+    console.log('listening on port', port);
+  });
+
+
+  app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+  });
